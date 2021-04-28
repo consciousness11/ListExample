8ab2ea8cae2f052886521eae82f29f14b06e7d2f
Marut <marut.dhungana@gmail.com>
2021-04-28 11:14:04 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> subjects = new List<string>();
+            int[] arr = new int[] { 2, 5, 6, -7, 8, 9, 1, 0 };
+
+            Quick q = new Quick();
+            q.Quick_Sort(arr, 0, arr.Length - 1);
+
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            subjects.Add("Programming");
+            subjects.Add("Database");
+            subjects.Add("Project");
+            subjects.Sort();
+            subjects.Remove("Project");
+           // Console.WriteLine(subjects.IndexOf("Programming"));
+
+            int x = subjects.BinarySearch("Project");
+          //  Console.WriteLine(x);
+
+
+            foreach(string s in subjects)
+            {
+           //     Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quick.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+   public class Quick
+    {
+        public  void Quick_Sort(int[] arr, int start, int end)
+        {
+            if (start < end)
+            {
+                int pivot = Partition(arr, start, end);
+
+                if (pivot > 1)
+                {
+                    Quick_Sort(arr, start, pivot - 1);
+                }
+                if (pivot + 1 < end)
+                {
+                    Quick_Sort(arr, pivot + 1, end);
+                }
+            }
+
+        }
+
+       public int Partition(int[] arr, int start, int end)
+        {
+            int pivot = arr[start];
+            while (true)
+            {
+
+                while (arr[start] < pivot)
+                {
+                    start++;
+                }
+
+                while (arr[end] > pivot)
+                {
+                    end--;
+                }
+
+                if (start < end)
+                {
+                    if (arr[start] == arr[end]) return end;
+
+                    int temp = arr[start];
+                    arr[start] = arr[end];
+                    arr[end] = temp;
+
+
+                }
+                else
+                {
+                    return end;
+                }
+            }
+        }
+    }
+}
